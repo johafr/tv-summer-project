f4a97e42c179b76df7ee66d2ec527b154463fee4
Johannes Finne Reinseth <johafr@stud.ntnu.no>
2022-06-20 16:15:48 +0200

    init interface

--- src/screens/Preview.tsx
+++ src/screens/Preview.tsx
@@ -1,5 +1,47 @@
+import { Button } from "@mui/material";
 import React from "react";
+import styled from "styled-components";
+import { IphoneScreen } from "../components/IphoneScreen";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 
 export const Preview: React.FC = () => {
-  return <div>Hei</div>;
+  return (
+    <ScreenDiv>
+      <IphoneScreen />
+      <Button
+        sx={{
+          position: "absolute",
+          top: "44%",
+          left: "35%",
+          "&:hover": {
+            backgroundColor: "#232333",
+          },
+        }}
+        disableRipple
+      >
+        <ArrowCircleLeftIcon />
+      </Button>
+      <Button
+        sx={{
+          position: "absolute",
+          top: "44%",
+          left: "58%",
+          marginLeft: 1,
+          "&:hover": {
+            backgroundColor: "#232333",
+          },
+        }}
+        disableRipple
+      >
+        <ArrowCircleRightIcon />
+      </Button>
+    </ScreenDiv>
+  );
 };
+
+const ScreenDiv = styled.div`
+  background-color: #232333;
+  min-height: 100vh;
+  color: white;
+`;
