9bf8f693078f2e72a2a43504f6b23841829ea0a6
thaffy <56458511+thaffy@users.noreply.github.com>
2022-06-21 12:50:10 +0200

    check on selected person name

---
+++ src/atoms/persons.ts
@@ -0,0 +1,23 @@
+import { atom } from "recoil";
+import { Person } from "./sentences";
+
+
+
+// Init data
+const initialData = [
+    {id : 1, name: 'Markus',color: 'rgb(132, 176, 214'},
+    {id : 2, name : 'Lisa', color: 'rgb(10, 214, 214'},
+  ]
+
+// State
+export const personsState = atom<Person[]>({
+    key: 'personsState',
+    default: initialData,
+})
+
+// Modifer (ie adding items to a list state)
+export const addPerson = (persons : Person[], newPerson : Person) => {
+    return (
+        [ ...persons,newPerson,]
+        )
+}
