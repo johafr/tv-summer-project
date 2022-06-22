a1ad056e37db6187b5dea6af5e31f53700bdd71c
Dontosh <me@torsteinstrand.com>
2022-06-22 16:00:54 +0200

    test component now logs userid logged in

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { auth } from "../Firebase";
 
 export const TestComponent: React.FC = (props) => {
+  console.log(auth.currentUser?.uid);
+
   return (
     <>
       <div>Bing bong!</div>
