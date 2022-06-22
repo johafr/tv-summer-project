aab37bc1a50a93b8a79301a23c69311a479a046d
Dontosh <me@torsteinstrand.com>
2022-06-22 11:22:05 +0200

    done

--- src/components/EditorTextInput.tsx
+++ src/components/EditorTextInput.tsx
@@ -6,90 +6,83 @@ import { inputTextState } from "../atoms/inputText";
 import { personsState } from "../atoms/persons";
 import { addSentence, sentencesState } from "../atoms/sentences";
 import { postJSON } from "../utils/postJSON";
+import * as S from "../styles/components/EditorTextInputStyles";
 
 // Component props
-type Props = {
-
-};
+type Props = {};
 
 // Component wrapper function
-export const EditorTextInput: React.FC<Props> = ({  }) => {
-    const [sentences,setSentences] = useRecoilState(sentencesState);
-    const [persons,setPersons] = useRecoilState(personsState);
-    const [inputText,setInputText] = useRecoilState(inputTextState);
-    const [inputName,setInputName] = useRecoilState(inputNameState);
-    const [changeSentenceText,setChangeSentenceText] = useRecoilState(changeSentenceTextState);
+export const EditorTextInput: React.FC<Props> = ({}) => {
+  const [sentences, setSentences] = useRecoilState(sentencesState);
+  const [persons, setPersons] = useRecoilState(personsState);
+  const [inputText, setInputText] = useRecoilState(inputTextState);
+  const [inputName, setInputName] = useRecoilState(inputNameState);
+  const [changeSentenceText, setChangeSentenceText] = useRecoilState(
+    changeSentenceTextState
+  );
 
-
-    const handleAddSentence = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const selectedPerson = persons.find((person) => inputName === person.name);
-        const newSentence = {
-          id: sentences[sentences.length - 1].id + 1,
-          person: selectedPerson,
-          content: inputText,
-        };
-        setSentences((currentSentences) =>
-          addSentence(currentSentences, newSentence)
-        );
-        setInputText("");
-    
-        await postJSON("http://localhost:4000/sentences", {
-          id: newSentence.id,
-          name: selectedPerson?.name,
-          text: newSentence.content,
-        });
-        console.log(sentences);
+  const handleAddSentence = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const selectedPerson = persons.find((person) => inputName === person.name);
+    const newSentence = {
+      id: sentences[sentences.length - 1].id + 1,
+      person: selectedPerson,
+      content: inputText,
     };
+    setSentences((currentSentences) =>
+      addSentence(currentSentences, newSentence)
+    );
+    setInputText("");
 
-    const handleChangeSentence = async (e: React.FormEvent, curID: number) => {
-        e.preventDefault();
-        const selectedSentenceIndex = sentences.findIndex(
-          (sentence) => sentence.id === curID
-        );
-        const selectedSentence = sentences[selectedSentenceIndex];
-        const updatedSentences = [
-          ...sentences.slice(0, selectedSentenceIndex),
-          { ...selectedSentence, content: changeSentenceText },
-          ...sentences.slice(selectedSentenceIndex + 1),
-        ];
-        setSentences(updatedSentences);
-    };
-
-
-
-
-    const listInputs = sentences.map((sentence) => {
-        return (
-          <form
-            key={sentence.id}
-            onSubmit={(e) => handleChangeSentence(e, sentence.id)}
-          >
-            <input
-              type="text"
-              defaultValue={sentence.content}
-              onChange={(e) => setChangeSentenceText(e.target.value)}
-            />
-          </form>
-        );
+    await postJSON("http://localhost:4000/sentences", {
+      id: newSentence.id,
+      name: selectedPerson?.name,
+      text: newSentence.content,
     });
+    console.log(sentences);
+  };
 
+  const handleChangeSentence = async (e: React.FormEvent, curID: number) => {
+    e.preventDefault();
+    const selectedSentenceIndex = sentences.findIndex(
+      (sentence) => sentence.id === curID
+    );
+    const selectedSentence = sentences[selectedSentenceIndex];
+    const updatedSentences = [
+      ...sentences.slice(0, selectedSentenceIndex),
+      { ...selectedSentence, content: changeSentenceText },
+      ...sentences.slice(selectedSentenceIndex + 1),
+    ];
+    setSentences(updatedSentences);
+  };
 
-
-
-
-    // Component end-return
+  const listInputs = sentences.map((sentence) => {
     return (
-        <div>
-            {listInputs}
-            <form onSubmit={(e) => handleAddSentence(e)}>
-              <input
-                type="text"
-                placeholder="Write a sentence..."
-                value={inputText}
-                onChange={(event) => setInputText(event.target.value)}
-              />
-            </form>
-        </div>
-    )
-}
+      <form
+        key={sentence.id}
+        onSubmit={(e) => handleChangeSentence(e, sentence.id)}
+      >
+        <S.FormInput
+          type="text"
+          defaultValue={sentence.content}
+          onChange={(e) => setChangeSentenceText(e.target.value)}
+        />
+      </form>
+    );
+  });
+
+  // Component end-return
+  return (
+    <div>
+      {listInputs}
+      <form onSubmit={(e) => handleAddSentence(e)}>
+        <S.FormInput
+          type="text"
+          placeholder="Write a sentence..."
+          value={inputText}
+          onChange={(event) => setInputText(event.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
---
+++ src/styles/screens/EditorStyles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const DisplayScreenContainer = styled.div`
+  position: relative;
+  left: 25rem;
+`;
+
+export const ParentDiv = styled.div`
+  display: flex;
+`;
+
+export const Editor = styled.div`
+  margin: 10px;
+  width: 25.5rem;
+  position: relative;
+`;
+
+export const TextForm = styled.div`
+  margin-top: 20px;
+  border-top: 1px inset #adadad;
+  border-left: 1px outset #adadad;
+  padding: 15px;
+  width: 500px;
+`;
