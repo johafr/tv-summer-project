8cebb3633f47f49cf6a7ff70f2b68c0decf0c736
Dontosh <me@torsteinstrand.com>
2022-06-22 09:53:04 +0200

    put styled components for every component into own file in own directory

--- src/components/DisplayScreen.tsx
+++ src/components/DisplayScreen.tsx
@@ -1,9 +1,9 @@
 import { Fab } from "@mui/material";
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import styled from "styled-components";
 import { activeIndex } from "../atoms/displayScreens";
 import { DisplayMeasurements, screenMeasurements } from "../atoms/measurements";
+import * as S from "../styles/components/DisplayScreenStyles";
 
 import {
   activePage,
@@ -36,7 +36,7 @@ export const DisplayScreen: React.FC = () => {
 
   return (
     <>
-      <Screen measurements={measurements}>
+      <S.Screen measurements={measurements}>
         <Fab
           size="small"
           onClick={handleGoLeft}
@@ -44,14 +44,14 @@ export const DisplayScreen: React.FC = () => {
         >
           <ArrowLeftIcon sx={{ color: "white" }} />
         </Fab>
-        <OutputScreen measurements={measurements}>
-          <Bump />
-          <ContentDiv measurements={measurements}>
+        <S.OutputScreen measurements={measurements}>
+          <S.Bump />
+          <S.ContentDiv measurements={measurements}>
             {activeScreen.map((card: sentenceCardProps) => (
               <SentenceCard name={card.name} text={card.text} />
             ))}
-          </ContentDiv>
-        </OutputScreen>
+          </S.ContentDiv>
+        </S.OutputScreen>
         <Fab
           size="small"
           onClick={handleGoRight}
@@ -59,48 +59,7 @@ export const DisplayScreen: React.FC = () => {
         >
           <ArrowRightIcon sx={{ color: "white" }} />
         </Fab>
-      </Screen>
+      </S.Screen>
     </>
   );
 };
-
-const OutputScreen = styled.div<{ measurements: DisplayMeasurements }>`
-  width: ${(props) => props.measurements.width}px;
-  height: ${(props) => props.measurements.height}px;
-  border-radius: 10px;
-  background-color: white;
-  margin-top: 0%;
-  border: 1px solid black;
-`;
-
-const Bump = styled.span`
-  position: absolute;
-  left: 50%;
-  width: 100px;
-  height: 15px;
-  margin-left: -50px;
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
-  background-color: #333333;
-`;
-
-const Screen = styled.div<{ measurements: DisplayMeasurements }>`
-  position: relative;
-  width: ${(props) => props.measurements.width + 100}px;
-  left: 50%;
-  margin-left: ${(props) => -props.measurements.width / 2 - 50}px;
-  height: 90vh;
-  justify-content: space-between;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-`;
-
-const ContentDiv = styled.div<{ measurements: DisplayMeasurements }>`
-  width: ${(props) => props.measurements.width - 20}px;
-  height: ${(props) => props.measurements.height - 30}px;
-  margin-top: 20px;
-  margin-left: 10px;
-  background-color: #d3d3d3;
-  color: black;
-`;
