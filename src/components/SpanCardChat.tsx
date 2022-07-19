c4478f5aed6c4abb88cb63a0aa3f5b6eafa81b8b
thaffy <56458511+thaffy@users.noreply.github.com>
2022-07-19 08:59:55 +0200

    Simple width-span design

---
+++ src/components/SpanCardChat.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+// Component props
+type Props = {
+    name : string;
+    text : string;
+
+};
+
+// Component wrapper function
+export const SpanCardChat: React.FC<Props> = ({name, text}) => {
+
+    // Component end-return
+    return (
+        <Wrapper>
+            <Name>{name}</Name>
+            <TextWrapper>
+                <p>{text}</p>
+            </TextWrapper>
+        </Wrapper>
+    )
+}
+
+export const Wrapper = styled.div`
+    text-align:center;
+`;
+
+export const Name = styled.h3`
+    margin-left:5px;
+    font-size: 0.7em;
+    text-align:center;
+`;
+
+export const TextWrapper = styled.div`
+    font-size: 0.8em;
+    padding: 2rem;
+    background-color:white;
+    flex-direction: column;
+    width:100%;
+
+    & p {
+        text-align:center;
+    }
+`;
