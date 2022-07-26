a6839fd6658e084938eecd6958e86b32ab6ccfd7
thaffy <56458511+thaffy@users.noreply.github.com>
2022-07-26 17:52:09 +0200

    refaktoring og plassering

--- src/components/editorComponents/EditorComponent.tsx
+++ src/components/editorComponents/EditorComponent.tsx
@@ -34,6 +34,7 @@ export const EditorComponent: React.FC = () => {
   const [viewPersonSelector, setViewPersonSelector] = useState<boolean[]>([
     false,
     false,
+    false,
   ]);
 
   const handleAddMessage = (
@@ -57,7 +58,7 @@ export const EditorComponent: React.FC = () => {
       ],
     };
     addMessage(newMessage);
-    console.log(newMessage);
+    console.log(type);
 
     // Set inputfield to empty
     const values = [...textInputs];
@@ -103,62 +104,216 @@ export const EditorComponent: React.FC = () => {
     <>
       <EditorNamesList numSelections={1} width={50} />
       <AddNewPersonInputField numSelections={1} />
+
+      {/* Wrapper for Editor Boxes + Output Screen */}
       <MainContainer>
-        <Wrapper>
-          <Expandable style={{ width: "100%" }}>
-            <div
-              style={{
-                marginTop: "6%",
-                border: "1px solid lightgray",
-                paddingTop: "1rem",
-                borderRadius: "100px",
-              }}
-            >
-              <ArticleIcon />
-              <p>Narrative</p>
+
+        {/* Wrapper for only the editor boxes */}
+        <Wrapper style={{ }} >
+
+          {/* Wrappers for individual components */}
+
+          <Expandable style={{}}>
+            <IconContainer style={{ }}>
+              <div>
+                <ArticleIcon />
+                <p>Narrative</p>
             </div>
+            </IconContainer>
+
+            <InputContainer style={{}}>
+              <ConvoName style={{border: 'none', textAlign: 'center'}}>NARRATIVE</ConvoName>
+              <form onSubmit={(event) => handleAddMessage(0, event, "NARRATIVE")}>
+                <TextInput
+                  value={textInputs[0][0].inputField}
+                  placeholder="Write a narrative..."
+                  onChange={(event) => handleChangeTextInput(0, event)}
+                />
+              </form>
+            </InputContainer>
+
           </Expandable>
-          <Expandable style={{ width: "100%" }}>
-            <div
-              style={{
-                marginTop: "9%",
-                border: "1px solid lightgray",
-                paddingTop: "1rem",
-                borderRadius: "100px",
-              }}
-            >
-              <ForumIcon />
-              <p>Conversation</p>
-            </div>
+
+
+           <Expandable style={{ }}>
+            <IconContainer style={{}}>
+              <div>
+                <ForumIcon />
+                <p>Conversation</p>
+              </div>
+            </IconContainer>
+
+            <InputContainer>
+              <ConvoName onClick={() => handleViewModal(0)}>
+                Replace with name
+                <PersonSelectModal viewModal={viewPersonSelector} side={0} />
+              </ConvoName>
+              <form
+                onSubmit={(event) => handleAddMessage(1, event, "DIALOG")}
+              >
+                <TextInput
+                  value={textInputs[1][0].inputField}
+                  placeholder="Write a dialogue..."
+                  onChange={(event) => handleChangeTextInput(1, event)}
+                />
+              </form>
+            </InputContainer>
+          
           </Expandable>
-          <Expandable style={{ width: "100%" }}>
-            <div
-              style={{
-                marginTop: "12%",
-                border: "1px solid lightgray",
-                paddingTop: "1rem",
-                borderRadius: "100px",
-              }}
-            >
-              <MessageIcon />
-              <p>Text Message</p>
-            </div>
+
+
+          <Expandable style={{ }}>
+            <IconContainer style={{}}>
+              <div>
+                <MessageIcon />
+                <p>Text Message</p>
+              </div>
+            </IconContainer>
+
+            <InputContainer>
+              <ConvoName onClick={() => handleViewModal(1)}>
+                Replace with name
+                <PersonSelectModal viewModal={viewPersonSelector} side={1} />
+              </ConvoName>
+              <form onSubmit={(event) => handleAddMessage(2, event, "TEXT")}>
+                <TextInput
+                  value={textInputs[2][0].inputField}
+                  placeholder="Write a text message..."
+                  onChange={(event) => handleChangeTextInput(2, event)}
+                />
+              </form>
+            </InputContainer>
+            
+
+
+
           </Expandable>
-          <Expandable style={{ width: "100%" }}>
-            <div
-              style={{
-                marginTop: "9%",
-                border: "1px solid lightgray",
-                paddingTop: "1rem",
-                borderRadius: "100px",
-              }}
-            >
-              <BubbleChartIcon />
-              <p>Thought</p>
-            </div>
-          </Expandable>
+          <Expandable style={{ }}>
+            <IconContainer style={{}}>
+              <div>
+                <BubbleChartIcon />
+                <p>Thought</p>
+              </div>
+            </IconContainer>
+
+            <InputContainer>
+              <ConvoName onClick={() => handleViewModal(2)}>
+                Replace with name
+                <PersonSelectModal viewModal={viewPersonSelector} side={2} />
+              </ConvoName>
+              <form onSubmit={(event) => handleAddMessage(3, event, "THOUGHT")}>
+                <TextInput
+                  value={textInputs[3][0].inputField}
+                  placeholder="Write a thought...."
+                  onChange={(event) => handleChangeTextInput(3, event)}
+                />
+              </form>
+            </InputContainer>
+
+          </Expandable> 
+
+
         </Wrapper>
-        <Wrapper>
+
+
+        <Output>
+          <h2 style={{ textAlign: "center", color: "gray" }}>OUTPUT</h2>
+          <div>
+            {currentPage?.messages.map((card: Message) => (
+              <InteractionSwitch
+                key={card.id}
+                id={card.id}
+                person={card.person}
+                content={card.content}
+                format={card.format}
+              />
+            ))}
+          </div>
+        </Output>
+      </MainContainer>
+    </>
+  );
+};
+
+export const MainContainer = styled.div`
+  padding-top: 5vh;
+  margin-top:20px;
+  width: 100%;
+  align-self: stretch;
+  display:flex;
+  flex-direction; column;
+  border: 1px dashed gray;
+`;
+
+export const Wrapper = styled.div`
+  width: 75%;
+`;
+
+export const Expandable = styled.div`
+  text-align: center;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: no-wrap;
+  flex-grow: 1;
+
+  & div {
+    margin: 2%;
+  }
+`;
+
+export const IconContainer = styled.div`
+  width: 20%;
+  border-radius: 20px;
+
+  & div {
+    margin-top: 25%;
+  }
+  & div:hover {
+    color:blue;
+    cursor: pointer;
+  }
+`;
+
+export const InputContainer = styled.div`
+  border: 1px solid gray;
+  border-radius: 5px;
+  width:80%
+`;
+
+export const TextInput = styled.input`
+  width: 98%;
+  height: 4rem;
+  border: none;
+  margin-bottom: 0.1rem;
+
+`;
+
+export const ConvoName = styled.h5`
+  width: 100%;
+  height: 1rem;
+  border-bottom: 1px solid gray;
+  text-align: left;
+  
+`;
+
+export const Output = styled.div`
+  width: 50%;
+  min-height: 100vh;
+  background-color: rgba(201, 228, 212);
+  box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.1);
+  margin-left: 1%;
+`;
+
+
+
+
+
+
+
+{/* Temp gravyard */}
+
+       {/*  <Wrapper>
           <Expandable>
             <div
               style={{
@@ -179,8 +334,8 @@ export const EditorComponent: React.FC = () => {
                 />
               </form>
             </div>
-          </Expandable>
-          <Expandable>
+          </Expandable> */}
+          {/* <Expandable>
             <div
               style={{
                 width: "30rem",
@@ -286,69 +441,5 @@ export const EditorComponent: React.FC = () => {
               <h4 style={{ color: "gray" }}>THOUGHT</h4>
               <TextInput placeholder="...." />
             </div>
-          </Expandable>
-        </Wrapper>
-
-        <Output>
-          <h2 style={{ textAlign: "center", color: "gray" }}>OUTPUT</h2>
-          <div>
-            {currentPage?.messages.map((card: Message) => (
-              <InteractionSwitch
-                key={card.id}
-                id={card.id}
-                person={card.person}
-                content={card.content}
-                format={card.format}
-              />
-            ))}
-          </div>
-        </Output>
-      </MainContainer>
-    </>
-  );
-};
-
-export const MainContainer = styled.div`
-  padding-top: 5vh;
-  display: flex;
-  width: 100%;
-  align-self: stretch;
-`;
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 1rem;
-`;
-
-export const TextInput = styled.input`
-  width: 19.6rem;
-  height: 4rem;
-  border: none;
-`;
-
-export const ConvoName = styled.h5`
-  width: 50%;
-  height: 2rem;
-  border-bottom: 1px solid lightgray;
-  padding: 4px;
-  &:hover {
-    background-color: lightgray;
-  }
-`;
-
-export const Output = styled.div`
-  width: 390px;
-  min-height: 844px;
-  background-color: rgba(201, 228, 212);
-  box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.1);
-  margin: auto;
-`;
-
-export const Expandable = styled.div`
-  padding-top: 0.5rem;
-  margin-top: 0.33rem;
-  margin-left: 0.2rem;
-  margin-bottom: 3rem;
-  text-align: center;
-`;
+          </Expandable> */}
+        {/* </Wrapper>  */}
--- src/components/editorComponents/PersonSelectModal.tsx
+++ src/components/editorComponents/PersonSelectModal.tsx
@@ -48,12 +48,12 @@ export const PersonSelectModal: React.FC<Props> = ({ viewModal, side }) => {
 };
 
 export const PersonModalWrapper = styled.div`
-  width: 100%;
+  width: 60%;
   padding: 0.5rem;
   background-color: lightgray;
   position: relative;
-  top: 30px;
-  left: 10px;
+  top: 0px;
+  left: 0px;
   box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   opacity: 0.8;
